72fc4294d155fd28839eea006e76e0b495ef4cbd
souLLe <af.nunosousa@gmail.com>
2025-10-06 16:42:41 +0100

    added class functions

--- game.cpp
+++ game.cpp
@@ -5,27 +5,102 @@ using namespace std;
 class Game {
     private:
         const int num_tiles = 90;
-        int num_players;
         int fall, crab, hell, death;
-        int fines[9];
-        int jumps[9];
+        int fines[5];
+        int jumps[5];
+
+        int player_fines[5] = {};
+        int num_players;
+        int nextPlayer;
+
+        int genRandInt(int lower, int higher) {
+            random_device rd;
+            mt19937 mt(rd());
+            uniform_int_distribution<int> randint(lower, higher);
+            
+            return randint(mt);
+        }
+
+        void drawBoard() {
+            for(int i = 0; i < 90; i += 10) {
+                if (i < 50) {
+                    fines[i/10] = i + genRandInt(1, 10);
+                    jumps[i/10] = i + genRandInt(1, 10);
+                }
+                else if (i < 60) {
+                    fall = i + genRandInt(1, 10);
+                }
+                else if (i < 70) {
+                    crab = i + genRandInt(1, 10);
+                }
+                else if (i < 80) {
+                    hell = i + genRandInt(1, 10);
+                }
+                else {
+                    death = i + genRandInt(1, 10);
+                }
+                
+            }
+
+        }
+
+        void finePlayer(int player) {
+            player_fines[player] = 1;
+        }
+
+        void applyJumo(int player) {
+
+        }
+
+        void applyFall(int player) {
+
+        }
+
+        void applyCrab(int player, int dice) {
+
+        }
+
+        void applyHell(int player) {
+
+        }
+
+        void applyDeath(int player) {
+            player_fines[player] = -1;
+        }
 
     public:
         Game(int num_players) {
             this->num_players = num_players;
+            drawBoard();
+
+            nextPlayer = 0;
+
         }
 
-        void drawBoard() {
-            random_device rd;
-            mt19937 mt(rd());
-            uniform_int_distribution<int> randint(1, 10);
+        int rollDice() {
+            return genRandInt(1, 6);
+        }
 
-            for(int i = 0; i < 90; i += 10) {
-                if (i < 50) {
-                    f
+        int getNextPlayer() {
+            int next = -1;
+            int index = nextPlayer;
+            while (next == -1) {
+                // fines > 0 -> reduce fines
+                // fines == 0 -> can play
+                // fines < 0 -> player is dead
+                if (player_fines[index] > 0) {
+                    player_fines[index] --;
                 }
-                int tile = randint(mt);
-                
+                else if (player_fines[index] == 0){
+                    next = index;
+                }
+                index ++;
+                if (index == num_players) {
+                    index = 0;
+                }
             }
+           
+            return next;
         }
+
 };
--- main.cpp
+++ main.cpp
@@ -1,27 +1,64 @@
 #include <iostream>
-#include <string.h>
 #include "game.cpp"
 
 using namespace std;
-int num_players;
 
-void runGame();
+const string HELP = "-1 - Exit\n0 / Enter - Continue Game\n1 - Print Status\n2 - Print Next Player\n3 - Print Board\n4 - Print this Menu\n";
+
+void processCommand(Game game, int command);
+void printStatus(Game game);
+void printNext(Game game);
+void printBoard(Game game);
+void nextPlay(Game game);
 
 int main (int argc, char *argv[]) {
-    cout << "How many players: ";
-    cin >> num_players;
+
+    int num_players = -1;
+    while ((std::cout << "How many players? ") && !(std::cin >> num_players)) {
+        cout << " !!! INVALID INPUT !!!\nPlease Insert Valid Number of Players: ";
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }   
 
     Game game = Game(num_players);
-    game.drawBoard();
     
-    string command;
-    cin >> command; 
+    int command = 0;   
+    cout << HELP;
     do {
-        runGame();
+        cout << "Enter command: ";
+        if (cin.get() == '\n') {
+            nextPlay(game);
+        }
+        cin >> command; 
+        processCommand(game, command);
+        
     }
-    while(command.compare("QUIT") == 0);
+    while(command != -1);
+
+    cout << "END OF GAME\n";
 }
 
-void runGame() {
+void processCommand(Game game, int command) {
+    switch(command) {
+        case 0: nextPlay(game);break;
+        case 1: printStatus(game);break;
+        case 2: printNext(game);break;
+        case 3: printBoard(game);break;
+        case 4: cout << HELP;break;
+        default: nextPlay(game);break;
+    }
+}
+
+void printStatus(Game game) {
+}
+void printNext(Game game) {
+}
+void printBoard(Game game) {
+}
+void nextPlay(Game game) {
+    int dice = game.rollDice();
+    int player = game.getNextPlayer();
+    cout << "Player "<< player << " rolled " << dice;
+
     
 }
